6610cf650d144f77ce434751471c20d7eadf76f3
Andre Watts <andrewattsii@gmail.com>
2019-04-24 18:11:33 -0400

    lab 6 dice roll

---
+++ lab_6/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace lab_6
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Grand Circus Casino ! Roll the dice ? y/n");
+            string yesno = Console.ReadLine();
+
+            if (yesno == "n")
+            {
+                goto tag;
+            }
+                while (yesno == "y")
+                {
+                
+                  
+                    Console.Write("how many sides do you want your dice to be ? ");
+                    int userDice = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Roll 1:");
+
+
+                     for (int i = 1; i < 3; i++)
+                     {
+                       int dice1 = random_class.diceOne(userDice);
+                       Console.WriteLine(dice1);
+                     }
+
+                    Console.WriteLine("Do youwant to roll again ? y/n");
+                    string noyes = Console.ReadLine();
+
+                    if(noyes == "n")
+                    {
+                     goto tag;
+                    }
+                }
+            
+
+            tag:
+            Console.WriteLine("Goodbye");
+
+
+
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+        
+    }
+}
---
+++ lab_6/random class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_6
+{
+    class random_class
+    {
+        public static int diceOne(int num1)
+        {
+            Random random = new Random();
+            return random.Next(1, num1);
+        }
+
+
+
+    }
+}
